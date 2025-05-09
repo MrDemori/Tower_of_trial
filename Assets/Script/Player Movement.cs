0cf808a18531ea3f03dff7a39af624b4dea49ec6
MrDemori <mrdemi1923@gmail.com>
2025-05-10 02:31:23 +0300

    Changes and adds
    Changed mechanic of double jump converted into dash mechanic
    Added level end point without trigger

--- Assets/Script/Player Movement.cs
+++ Assets/Script/Player Movement.cs
@@ -9,9 +9,16 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.1f;
+    [SerializeField] private float inDashTime = 1f;
+    [SerializeField] private float dashSpeed = 1f;
 
-    private bool doubleJump = true;
+    private bool dash = true;
     private bool isGrounded;
+    private bool isDashing;
+    private float dashTimer;
+    private int moveInput;
+    private int verticalInput;
+    private Vector2 dashDirection;
 
     void Start()
     {
@@ -20,28 +27,70 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float moveAmount = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveAmount * moveSpeed, rb.linearVelocity.y);
+        if (!isDashing)
+        {
+            moveInput = (int)Input.GetAxisRaw("Horizontal");
+            verticalInput = (int)Input.GetAxisRaw("Vertical");
+        }
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            doubleJump = true;
+            dash = true;
         }
         else
         {
-            if (Input.GetButtonDown("Jump") && doubleJump)
+            if (Input.GetButtonDown("Jump") && dash)
             {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-                doubleJump = false;
+                Vector2 inputDir = new Vector2(moveInput, verticalInput);
+                if (inputDir != Vector2.zero)
+                {
+                    dashDirection = inputDir.normalized;
+                    dashStart();
+                }
             }
         }
-        
+    }
+
+    void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            rb.linearVelocity = dashDirection * dashSpeed;
+            dashTimer -= Time.deltaTime;
+            if(dashTimer <= 0f)
+            {
+                dashEnd();
+            }
+        }
+    }
+
+    void dashStart()
+    {
+        isDashing = true;
+        dashTimer = inDashTime;
+        dash = false;
+    }
+
+    void dashEnd()
+    {
+        isDashing = false;
+        rb.linearVelocity = Vector2.zero;
     }
 
     void OnTriggerEnter2D()
     {
-        doubleJump = true;
+        dash = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        dash = false;
+        if (isDashing)
+        {
+            dashEnd();
+        }
     }
 
 }
